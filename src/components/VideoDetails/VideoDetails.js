1b5f4eebac68c0e1ae6bb432c01c508dd00f071e
unknown <jackytam2020@gmail.com>
2022-11-08 11:28:01 -0800

    added api functionality

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -3,13 +3,15 @@ import './VideoDetails.scss';
 import ViewsLogo from '../../assets/icons/views.svg';
 import LikesLogo from '../../assets/icons/likes.svg';
 
+import CommentSection from '../CommentSection/CommentSection';
+
 function VideoDetails(props) {
+  console.log(props);
   return (
     <section className="video-details">
       <div className="video-details__title-holder">
         <h1 className="video-details__title">{props.videoTitle}</h1>
       </div>
-
       <div className="video-details__metrics">
         <div className="video-details__metrics-left">
           <p className="video-details__channel">By {props.channel}</p>
@@ -34,8 +36,8 @@ function VideoDetails(props) {
           </div>
         </div>
       </div>
-
       <p className="video-details__description">{props.description}</p>
+      {/* <CommentSection numOfComments={props.}/> */}
     </section>
   );
 }
---
+++ src/pages/UploadPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './UploadPage.scss';
+
+function UploadPage(props) {
+  return <div></div>;
+}
+
+export default UploadPage;
